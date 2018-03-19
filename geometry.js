d198caddbe3490f4a662110b5657c728b045eb05
Zach Stevens <zachstevens39@gmail.com>
2018-03-19 10:39:35 -0600

    Split into multiple files

--- geometry.js
+++ geometry.js
@@ -1,7 +1,3 @@
-// Geometry stuff
+const volume = require('./volumes')
 
-function torusVolume(majorRadius, minorRadius) {
-  return (Math.PI * minorRadius * minorRadius) * (2 * Math.PI * majorRadius)
-}
-
-console.log(torusVolume(8, 7))
+console.log(volume(8, 7))
---
+++ volumes.js
@@ -0,0 +1,7 @@
+// function torusVolume(majorRadius, minorRadius) {
+//   return (Math.PI * (minorRadius ** 2)) * (2 * Math.PI * majorRadius)
+// }
+
+module.exports = function (majorRadius, minorRadius) {
+  return (Math.PI * (minorRadius ** 2)) * (2 * Math.PI * majorRadius)
+}
